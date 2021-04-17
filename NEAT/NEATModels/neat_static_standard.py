40a19a666fb172ffa961911d09b101064fda6dc7
kapoorlab <randomaccessiblekapoor@gmail.com>
2021-04-17 18:30:27 +0200

    yolo v0 v1 static training

--- NEAT/NEATModels/neat_static_standard.py
+++ NEAT/NEATModels/neat_static_standard.py
@@ -12,7 +12,7 @@ from NEATUtils import helpers
 from keras import callbacks
 import os
 from NEATModels import nets
-from NEATModels.loss import static_yolo_loss, yolo_loss_v1, yolo_loss_v0
+from NEATModels.loss import static_yolo_loss, yolo_loss_v0
 from keras import backend as K
 #from IPython.display import clear_output
 from keras import optimizers
