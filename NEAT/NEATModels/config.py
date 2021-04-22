dc487f5a36e3f8f95df9549d2380033f309db53d
kapoorlab <randomaccessiblekapoor@gmail.com>
2021-04-22 14:04:24 +0200

    train config

--- NEAT/NEATModels/config.py
+++ NEAT/NEATModels/config.py
@@ -10,13 +10,25 @@ import numpy as np
 
 class NeatConfig(argparse.Namespace):
     
-    def __init__(self, residual = True, simple = False, catsimple = False, depth = 29, start_kernel = 7, mid_kernel = 3, startfilter = 48, lstm = 16, epochs =100, learning_rate = 1.0E-4, batch_size = 10, ModelName = 'NEATModel',  **kwargs):
+    def __init__(self, npz_directory = None, npz_name = None, npz_val_name = None, key_categories = None, key_cord = None,  residual = True, imagex = 128, imagey = 128, imaget = 9, nboxes = 1,
+                 depth = 29, start_kernel = 7, mid_kernel = 3, startfilter = 48, lstm = 16, epochs =100, 
+                 learning_rate = 1.0E-4, batch_size = 10, model_name = 'NEATModel', yolo_v0 = True, yolo_v1 = False, yolo_v2 = False, multievent = True,  **kwargs):
         
         
-           
+           self.npz_directory = npz_directory
+           self.npz_name = npz_name
+           self.npz_val_name = npz_val_name
+           self.key_categories = key_categories
+           self.key_cord = key_cord
            self.residual = residual
-           self.simple = simple
-           self.catsimple = catsimple
+           self.yolo_v0 = yolo_v0
+           self.yolo_v1 = yolo_v1
+           self.yolo_v2 = yolo_v2
+           self.categories = len(self.key_categories)
+           self.box_vector = len(self.key_cord)
+           self.imagex = imagex
+           self.imagey = imagey
+           self.imaget = imaget
            self.depth = depth
            self.start_kernel = start_kernel
            self.mid_kernel = mid_kernel
@@ -25,17 +37,23 @@ class NeatConfig(argparse.Namespace):
            self.epochs = epochs
            self.learning_rate = learning_rate
            self.batch_size = batch_size
-           self.ModelName = ModelName
+           self.model_name = model_name
            self.is_valid()
     
 
     def to_json(self):
 
          config = {
-                 'ModelName' : self.ModelName,
+                 'model_name' : self.model_name,
                  'residual' : self.residual,
-                 'simple' : self.simple,
-                 'catsimple' : self.catsimple,
+                 'multievent' : self.multievent,
+                 'yolo_v0': self.yolo_v0,
+                 'yolo_v1': self.yolo_v1,
+                 'yolo_v2': self.yolo_v2,
+                 'imagex' : self.imagex,
+                 'imagey' : self.imagey,
+                 'imaget' : self.imaget,
+                 'nboxes' : self.nboxes,
                  'depth' : self.depth,
                  'start_kernel' : self.start_kernel,
                  'mid_kernel' : self.mid_kernel,
@@ -45,6 +63,15 @@ class NeatConfig(argparse.Namespace):
                  'learning_rate' : self.learning_rate,
                  'batch_size' : self.batch_size
                  }
+         
+         for (k,v) in self.key_categories.items():
+             config[k] = v
+             
+         for (k,v) in self.key_cord.items():
+             config[k] = v
+             
+             
+             
          return config
          
          
@@ -66,16 +93,25 @@ class NeatConfig(argparse.Namespace):
 
             ok = {}
             ok['residual'] = isinstance(self.residual,bool)
-            ok['simple'] = isinstance(self.simple,bool)
-            ok['catsimple'] = isinstance(self.catsimple,bool)
+            ok['yolo_v0'] = isinstance(self.yolo_v0,bool)
+            ok['yolo_v1'] = isinstance(self.yolo_v1,bool)
+            ok['yolo_v2'] = isinstance(self.yolo_v2,bool)
             ok['depth']         = _is_int(self.depth,1)
             ok['start_kernel']       = _is_int(self.start_kernel,1)
             ok['mid_kernel']         = _is_int(self.mid_kernel,1)
             ok['startfilter']        = _is_int(self.startfilter, 1)
             ok['lstm']         = _is_int(self.lstm,1)
             ok['epochs']        = _is_int(self.epochs, 1)
+            ok['nboxes']       = _is_int(self.nboxes, 1)
+            
+            ok['imagex'] = _is_int(self.imagex, 1)
+            ok['imagey'] = _is_int(self.imagey, 1)
+            ok['imaget'] = _is_int(self.imaget, 1)
+            
             ok['learning_rate'] = np.isscalar(self.learning_rate) and self.learning_rate > 0
-    
+            ok['multievent'] = isinstance(self.multievent,bool)
+            ok['categories'] =  _is_int(len(self.key_categories), 1)
+            ok['box_vector'] =  _is_int(self.box_vector, 1)
             
     
             if return_invalid:
