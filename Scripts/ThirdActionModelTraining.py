8770050ec485daa728a29a85e50217a5ddb13d7b
kapoorlab <randomaccessiblekapoor@gmail.com>
2021-05-16 20:28:28 +0200

    scripts

---
+++ Scripts/ThirdActionModelTraining.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import sys
+import os
+from glob import glob
+sys.path.append("../NEAT")
+from NEATModels import NEATDynamic, nets
+from NEATModels.config import dynamic_config
+from NEATUtils import helpers
+from NEATUtils.helpers import save_json, load_json
+os.environ["CUDA_VISIBLE_DEVICES"]="2"
+os.environ["HDF5_USE_FILE_LOCKING"] = "FALSE"
+
+
+# In[2]:
+
+
+npz_directory = '/data/u934/service_imagerie/v_kapoor/FinalONEATTraining/Microneatsmallbin2/'
+npz_name = 'microneatsmallbin2.npz'
+npz_val_name = 'microneatsmallbin2val.npz'
+
+#Read and Write the h5 file, directory location and name
+model_dir =  '/data/u934/service_imagerie/v_kapoor/FinalONEATTraining/Microneatmodel/'
+model_name = 'microseqnetbin2d74.h5'
+
+#Neural network parameters
+division_categories_json = model_dir + 'MicroscopeCategories.json'
+key_categories = load_json(division_categories_json)
+division_cord_json = model_dir + 'MicroscopeCord.json'
+key_cord = load_json(division_cord_json)
+
+#For ORNET use residual = True and for OSNET use residual = False
+residual = False
+#NUmber of starting convolutional filters, is doubled down with increasing depth
+startfilter = 48
+#CNN network start layer, mid layers and lstm layer kernel size
+start_kernel = 3
+lstm_kernel = 3
+mid_kernel = 3
+#Network depth has to be 9n + 2, n= 3 or 4 is optimal for Notum dataset
+depth = 74
+#Size of the gradient descent length vector, start small and use callbacks to get smaller when reaching the minima
+learning_rate = 1.0E-6
+#For stochastic gradient decent, the batch size used for computing the gradients
+batch_size = 8
+# use softmax for single event per box, sigmoid for multi event per box
+lstm_hidden_unit = 16
+#Training epochs, longer the better with proper chosen learning rate
+epochs = 250
+nboxes = 1
+#The inbuilt model stride which is equal to the nulber of times image was downsampled by the network
+show = False
+
+size_tminus = 3
+size_tplus = 0
+imagex = 64
+imagey = 64
+
+# In[4]:
+
+
+config = dynamic_config(npz_directory =npz_directory, npz_name = npz_name, npz_val_name = npz_val_name, 
+                         key_categories = key_categories, key_cord = key_cord, nboxes = nboxes, imagex = imagex,
+                         imagey = imagey, size_tminus = size_tminus, size_tplus =size_tplus, 
+                         residual = residual, depth = depth, start_kernel = start_kernel, mid_kernel = mid_kernel,
+                         lstm_kernel = lstm_kernel, lstm_hidden_unit = lstm_hidden_unit, show = show,
+                         startfiler = startfilter, batch_size = batch_size, model_name = model_name)
+
+config_json = config.to_json()
+
+print(config)
+save_json(config_json, model_dir + os.path.splitext(model_name)[0] + '_Parameter.json')
+# In[ ]:
+
+
+Train = NEATDynamic(config, model_dir, model_name)
+
+Train.loadData()
+
+Train.TrainModel()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
