e4645fa24f1a3d968552bd7692b9f07b35a83e4d
kapoorlab <randomaccessiblekapoor@gmail.com>
2021-07-02 16:53:10 +0200

    rectangle predictions

--- Scripts/MicroscopePrediction.py
+++ Scripts/MicroscopePrediction.py
@@ -19,11 +19,11 @@ os.environ["HDF5_USE_FILE_LOCKING"] = "FALSE"
 
 # In[ ]:
 
-Z_imagedir = '/data/u934/service_imagerie/v_kapoor/CurieTrainingDatasets/Raw_Datasets/BorialisS1S2/Test/GT/'
-#Z_imagedir = '/data/u934/service_imagerie/v_kapoor/FinalONEATTraining/Z_ONEAT_fly_test/'
+#Z_imagedir = '/data/u934/service_imagerie/v_kapoor/CurieTrainingDatasets/Raw_Datasets/BorialisS1S2/Test/GT/'
+Z_imagedir = '/data/u934/service_imagerie/v_kapoor/FinalONEATTraining/Z_ONEAT_fly_test/'
 imagedir = '/data/u934/service_imagerie/v_kapoor/FinalONEATTraining/ONEAT_fly_test/'
 model_dir =  '/data/u934/service_imagerie/v_kapoor/FinalONEATTraining/Microneatmodel/'
-model_name = 'microseqnetbin2d56'
+model_name = 'microseqnetbin2d65'
 projection_model_name = 'projectionmodelbin2'
 division_categories_json = model_dir + 'MicroscopeCategories.json'
 catconfig = load_json(division_categories_json)
@@ -33,16 +33,16 @@ fileextension = '*tif'
 
 
 model = NEATPredict(None, model_dir , model_name,catconfig, cordconfig)
-projection_model = ProjectionCARE(config = None, name = projection_model_name, basedir = model_dir)
+projection_model = None #ProjectionCARE(config = None, name = projection_model_name, basedir = model_dir)
 
 
 
 n_tiles = (1,1)
-Z_n_tiles = (1,4,4)
-event_threshold = 0.9999
+Z_n_tiles = (1,1,1)
+event_threshold = 1
 iou_threshold = 0.01
-nb_predictions = 100
-downsample = True
+nb_predictions = 10
+downsample = False
 
 
 model.predict(imagedir, {}, {}, Z_imagedir, [], [], 0, 0, fileextension = fileextension, nb_prediction = nb_predictions, downsample = downsample, n_tiles = n_tiles, Z_n_tiles = Z_n_tiles, event_threshold = event_threshold, iou_threshold = iou_threshold, projection_model = projection_model)
