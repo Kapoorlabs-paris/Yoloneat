6311ea6db3a9815d66e1269a280510dcf146a6b4
kapoorlab <randomaccessiblekapoor@gmail.com>
2021-07-04 13:19:26 +0200

    div model

--- Scripts/DivisionDetection.py
+++ Scripts/DivisionDetection.py
@@ -14,6 +14,8 @@ from NEATModels.config import dynamic_config
 from NEATUtils import helpers
 from NEATUtils.helpers import load_json
 from stardist.models import StarDist2D
+from csbdeep.models import Config, CARE
+
 os.environ["CUDA_VISIBLE_DEVICES"]="2"
 os.environ["HDF5_USE_FILE_LOCKING"] = "FALSE"
 from pathlib import Path
@@ -22,22 +24,22 @@ from pathlib import Path
 # In[2]:
 
 
-imagedir = '/home/sancere/Kepler/FinalONEATTraining/NEATTest/'
-model_dir = '/home/sancere/Kepler/FinalONEATTraining/PreMicroneatmodel/'
-savedir= '/home/sancere/Kepler/FinalONEATTraining/NEATTest/Save/'
-markerdir = '/home/sancere/Kepler/FinalONEATTraining/NEATTest/Save/Markers/'
-model_name = 'premicroseqnetbin2d38lstm8'
-star_model_name = '/home/sancere/Kepler/FinalONEATTraining/Everydayneatmodel/bin2stardist'
-division_categories_json = model_dir + 'MicroscopeCategories.json'
+imagedir = '/data/u934/service_imagerie/v_kapoor/FinalONEATTraining/NEATTest/'
+model_dir = '/data/u934/service_imagerie/v_kapoor/FinalONEATTraining/EverydayneatmodelV1/'
+savedir= '/data/u934/service_imagerie/v_kapoor/FinalONEATTraining/NEATTest/MarkerNineFramePrediction/'
+markerdir = '/data/u934/service_imagerie/v_kapoor/FinalONEATTraining/NEATTest/MarkerNineFramePrediction/Markers/'
+model_name = 'divisionm4d65'
+unet_model_name = '/data/u934/service_imagerie/v_kapoor/FinalONEATTraining/Everydayneatmodel/UNETDrosophilaMembrane/'
+division_categories_json = model_dir + 'DivisionCategories.json'
 catconfig = load_json(division_categories_json)
-division_cord_json = model_dir + 'MicroscopeCord.json'
+division_cord_json = model_dir + 'DivisionCord.json'
 cordconfig = load_json(division_cord_json)
 model = NEATDynamic(None, model_dir , model_name,catconfig, cordconfig)
-starmodel = StarDist2D(config = None, name = star_model_name, basedir = model_dir)
+unetmodel = CARE(config = None, name = unet_model_name, basedir = model_dir)
 Path(savedir).mkdir(exist_ok=True)
 n_tiles = (1,1)
-event_threshold = 1.0 - 1.0E-7
-iou_threshold = 0.6
+event_threshold = 0.99
+iou_threshold = 0.01
 yolo_v2 = False
 
 
@@ -53,28 +55,12 @@ marker_dict = {}
 for imagename in X:
      markers, markers_tree, density_location =  model.get_markers(imagename, starmodel,savedir, n_tiles = n_tiles, markerdir = markerdir)
      
-     marker_dict[imagename] = [markers, markers_tree, density_location]
-     
-
-
-# # In the cell below use the computed dictionary and apply division model detection
-
-# In[ ]:
-
-
-Raw_path = os.path.join(imagedir, '*tif')
-X = glob.glob(Raw_path)
-for imagename in X:
-     
-     markers, markers_tree, density_location = marker_dict[imagename]
      model.predict(imagename,markers, markers_tree, density_location, savedir, n_tiles = n_tiles, event_threshold = event_threshold, iou_threshold = iou_threshold)
 
 
 # In[3]:
 
 
-
-model.showNapari(imagedir, savedir, yolo_v2)
 
 
 # In[ ]:
