4909450d70decf06656879b6fdf0e43eafe00d2c
kapoorlab <randomaccessiblekapoor@gmail.com>
2021-07-07 15:54:00 +0200

    training models

--- NEAT/NEATModels/neat_standard.py
+++ NEAT/NEATModels/neat_standard.py
@@ -211,7 +211,7 @@ class NEATDynamic(object):
         
         
         Path(self.model_dir).mkdir(exist_ok=True)
-        
+       
         
         if self.yolo_v2:
             
@@ -248,9 +248,10 @@ class NEATDynamic(object):
         print(self.box_vector)
         dummyY = np.zeros([self.Y.shape[0],self.Y.shape[1],self.Y.shape[2],self.categories + self.nboxes* self.box_vector])
         dummyY[:,:,:,:self.Y.shape[3]] = self.Y
-        
+       
         dummyY_val = np.zeros([self.Y_val.shape[0],self.Y_val.shape[1],self.Y_val.shape[2],self.categories + self.nboxes* self.box_vector])
         dummyY_val[:,:,:,:self.Y_val.shape[3]] = self.Y_val
+        
         for b in range(1, self.nboxes):
             
             dummyY[:,:,:,self.categories + b * self.box_vector:self.categories + (b + 1) * self.box_vector] = self.Y[:,:,:, self.categories: self.categories + self.box_vector]
---
+++ Scripts/MicroscopePredictionRes.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import sys
+import os
+from glob import glob
+sys.path.append("../NEAT")
+from NEATModels import NEATPredict, nets
+from NEATModels.config import dynamic_config
+from NEATUtils import helpers
+from NEATUtils.helpers import load_json
+from csbdeep.models import ProjectionCARE
+os.environ["CUDA_VISIBLE_DEVICES"]="3"
+os.environ["HDF5_USE_FILE_LOCKING"] = "FALSE"
+
+
+# In[ ]:
+
+#Z_imagedir = '/data/u934/service_imagerie/v_kapoor/CurieTrainingDatasets/Raw_Datasets/BorialisS1S2/Test/GT/'
+Z_imagedir = '/data/u934/service_imagerie/v_kapoor/FinalONEATTraining/Z_ONEAT_fly_test/'
+imagedir = '/data/u934/service_imagerie/v_kapoor/FinalONEATTraining/ResONEAT_fly_test/'
+model_dir =  '/data/u934/service_imagerie/v_kapoor/FinalONEATTraining/Microneatmodel/'
+model_name = 'microresnetbin2d47'
+projection_model_name = 'projectionmodelbin2'
+division_categories_json = model_dir + 'MicroscopeCategories.json'
+catconfig = load_json(division_categories_json)
+division_cord_json = model_dir + 'MicroscopeCord.json'
+cordconfig = load_json(division_cord_json)
+fileextension = '*TIF'
+
+
+model = NEATPredict(None, model_dir , model_name,catconfig, cordconfig)
+projection_model = ProjectionCARE(config = None, name = projection_model_name, basedir = model_dir)
+ 
+
+
+
+n_tiles = (1,1)
+Z_n_tiles = (1,1,1)
+event_threshold = 1
+iou_threshold = 0.001
+nb_predictions = 200
+downsample = False
+
+
+model.predict(imagedir, {}, {}, Z_imagedir, [], [], 0, 0, fileextension = fileextension, nb_prediction = nb_predictions, downsample = downsample, n_tiles = n_tiles, Z_n_tiles = Z_n_tiles, event_threshold = event_threshold, iou_threshold = iou_threshold, projection_model = projection_model)
+
