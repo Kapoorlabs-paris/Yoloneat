5d4add232d138775b16b6f7f3cc4ebcfae5759b0
kapoorlab <randomaccessiblekapoor@gmail.com>
2021-08-11 19:35:51 +0200

    focus

--- NEAT/NEATModels/nets.py
+++ NEAT/NEATModels/nets.py
@@ -755,8 +755,8 @@ def resnet_3D_v2(input_shape, categories,box_vector, stage_number = 3, last_conv
       
     
 
-    output_cat = (Conv3D(categories, (round(input_shape[0]/last_conv_factor),round(input_shape[1]/last_conv_factor), round(input_shape[2]/last_conv_factor)),activation= last_activation ,kernel_regularizer=regularizers.l2(reg_weight), padding = 'valid'))(input_cat)
-    output_box = (Conv3D((box_vector), (round(input_shape[0]/last_conv_factor),round(input_shape[1]/last_conv_factor), round(input_shape[2]/last_conv_factor)),activation= 'sigmoid' ,kernel_regularizer=regularizers.l2(reg_weight), padding = 'valid'))(input_box)
+    output_cat = (Conv3D(categories, (round(input_shape[1]/last_conv_factor),round(input_shape[2]/last_conv_factor), round(input_shape[3]/last_conv_factor)),activation= last_activation ,kernel_regularizer=regularizers.l2(reg_weight), padding = 'valid'))(input_cat)
+    output_box = (Conv3D((box_vector), (round(input_shape[1]/last_conv_factor),round(input_shape[2]/last_conv_factor), round(input_shape[3]/last_conv_factor)),activation= 'sigmoid' ,kernel_regularizer=regularizers.l2(reg_weight), padding = 'valid'))(input_box)
     
 
     block = Concat(-1)
@@ -799,7 +799,7 @@ def seqnet_3D_v2(input_shape, categories, box_vector, stage_number = 3, last_con
     # Returns
         model (Model): Keras model instance
     """
-    img_input = layers.Input(shape = (None, None, None, input_shape[3]))
+    img_input = layers.Input(shape = (None, None, None, None))
     if (depth - 2) % 9 != 0:
         raise ValueError('depth should be 9n+2 (eg 56 or 110 in [b])')
     # Start model definition.
@@ -858,8 +858,8 @@ def seqnet_3D_v2(input_shape, categories, box_vector, stage_number = 3, last_con
       
     
 
-    output_cat = (Conv3D(categories, (round(input_shape[0]/last_conv_factor),round(input_shape[1]/last_conv_factor), round(input_shape[2]/last_conv_factor)),activation= last_activation ,kernel_regularizer=regularizers.l2(reg_weight), padding = 'valid'))(input_cat)
-    output_box = (Conv3D((box_vector), (round(input_shape[0]/last_conv_factor),round(input_shape[1]/last_conv_factor), round(input_shape[2]/last_conv_factor)),activation= 'sigmoid' ,kernel_regularizer=regularizers.l2(reg_weight), padding = 'valid'))(input_box)
+    output_cat = (Conv3D(categories, (round(input_shape[1]/last_conv_factor),round(input_shape[2]/last_conv_factor), round(input_shape[3]/last_conv_factor)),activation= last_activation ,kernel_regularizer=regularizers.l2(reg_weight), padding = 'valid'))(input_cat)
+    output_box = (Conv3D((box_vector), (round(input_shape[1]/last_conv_factor),round(input_shape[2]/last_conv_factor), round(input_shape[3]/last_conv_factor)),activation= 'sigmoid' ,kernel_regularizer=regularizers.l2(reg_weight), padding = 'valid'))(input_box)
     
 
     block = Concat(-1)
