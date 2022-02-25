edc687abf7217b43f80aa5c50540fa96a49cb2bb
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-02-25 14:20:51 +0100

    norm fix

--- NEAT/NEATModels/neat_goldstandard.py
+++ NEAT/NEATModels/neat_goldstandard.py
@@ -576,7 +576,7 @@ class NEATDynamic(object):
             if event_label > 0:
 
                #best_sorted_event_box = self.classedboxes[event_name][0]
-               best_sorted_event_box = dynamic_nms(self.heatmap,self.maskimage, self.originalimage, self.classedboxes, event_name, event_label, self.downsamplefactor, self.iou_threshold, self.event_threshold, self.imagex, self.imagey, self.imaget, self.thresh )
+               best_sorted_event_box = dynamic_nms(self.heatmap,self.maskimage, self.originalimage, self.classedboxes, event_name, event_label, self.downsample, self.iou_threshold, self.event_threshold, self.imagex, self.imagey, self.imaget, self.thresh )
 
                best_iou_classedboxes[event_name] = [best_sorted_event_box]
 
