50a590c26c1a76a3e9792702f6450d78cbd4d30f
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-03-05 18:25:32 +0100

    clean_marker

--- setup.py
+++ setup.py
@@ -1,63 +1,42 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jul  7 14:28:22 2020
+import setuptools
+from setuptools import find_packages, setup
 
-@author: aimachine
-"""
-
-from __future__ import absolute_import
-from setuptools import setup, find_packages
-from os import path
-
-_dir = path.abspath(path.dirname(__file__))
-
-with open(path.join(_dir,'version.py')) as f:
-    exec(f.read())
-
-with open(path.join(_dir,'README.md')) as f:
+with open('README.md') as f:
     long_description = f.read()
 
 
-setup(name='ONT',
-      version=__version__,
-      description='ONEAT-Online Offline network for event aware topology detection (ONEAT)',
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      url='',
-      author='Varun Kapoor',
-      author_email='vkapoor@curie.fr, randomaccessiblekapoor@gmail.com',
-      license='BSD 3-Clause License',
-      packages=find_packages(),
+setup(
+    name="oneat",
 
-      project_urls={
-          'Documentation': '',
-          'Repository': 'https://github.com/kapoorlab/ONT',
-      },
+    version='1.0.0',
 
-      classifiers=[
-          'Development Status :: 4 - Beta',
-          'Intended Audience :: Science/Research',
-          'Topic :: Scientific/Engineering',
-          'License :: OSI Approved :: BSD License',
-
-          'Programming Language :: Python :: 3.5',
-          'Programming Language :: Python :: 3.6',
-          'Programming Language :: Python :: 3.7',
-      ],
-
-      install_requires=[
-          "numpy",
-          "scipy",
-          "matplotlib",
-          "six",
-          "keras>=2.1.2,<2.4",
-          "h5py",
-          "imagecodecs-lite<=2020; python_version<'3.6'",
-          "tifffile",
-          "tqdm",
-          "elasticdeform"
-          "pathlib2; python_version<'3'",
-          "backports.tempfile; python_version<'3.4'",
-      ]
-      )
+    author='Varun Kapoor',
+    author_email='randomaccessiblekapoor@gmail.com',
+    url='https://github.com/Kapoorlabs-paris/Yoloneat/',
+    description='Static and Dynamic classification tool.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        
+        "pandas",
+        "vollseg",
+        "numpy==1.20.0",
+        "scipy",
+        "tifffile",
+        "matplotlib",
+        "imagecodecs",
+        "napari",
+        "diplib",
+        "opencv-python" 
+       
+    ],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
