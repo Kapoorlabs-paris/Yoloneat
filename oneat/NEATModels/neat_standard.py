1fee376d6b79a53f224ce8b90622fbeb7f578cd8
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-03-07 18:34:19 +0100

    fix code

--- oneat/NEATModels/neat_standard.py
+++ oneat/NEATModels/neat_standard.py
@@ -46,13 +46,12 @@ class NEATDynamicSeg(NEATDynamic):
 
     
 
-    
 
     def predict_standard(self, imagename, markers, marker_tree, savedir, n_tiles=(1, 1), overlap_percent=0.8,
-                event_threshold=0.5, iou_threshold=0.1, fidelity = 5, downsamplefactor = 1, maskimage = None, maskfilter = 10):
+                event_threshold=0.5, iou_threshold=0.1, fidelity = 5, downsamplefactor = 1, watershed = None, maskimage = None, maskfilter = 10):
 
         self.predict(imagename,savedir,n_tiles = n_tiles, overlap_percent = overlap_percent, event_threshold = event_threshold, iou_threshold = iou_threshold, 
-        fidelity = fidelity, downsamplefactor = downsamplefactor, maskimage = maskimage, maskfilter = maskfilter, markers = markers, marker_tree = marker_tree,
+        fidelity = fidelity, downsamplefactor = downsamplefactor,  maskfilter = maskfilter, markers = markers, marker_tree = marker_tree, watershed = watershed, maskimage = maskimage,
          remove_markers = None )
 
 def CreateVolume(patch, imaget, timepoint):
